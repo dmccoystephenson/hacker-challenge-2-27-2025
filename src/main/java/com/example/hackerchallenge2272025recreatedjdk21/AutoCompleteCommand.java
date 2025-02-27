4749681d26be19f948cba92c6e726a27b96547cd
dmccoystephenson <dmccoystephenson@gmail.com>
2025-02-27 14:56:23 -0700

    Added logging

--- src/main/java/com/example/hackerchallenge2272025recreatedjdk21/AutoCompleteCommand.java
+++ src/main/java/com/example/hackerchallenge2272025recreatedjdk21/AutoCompleteCommand.java
@@ -2,10 +2,12 @@ package com.example.hackerchallenge2272025recreatedjdk21;
 
 import java.util.ArrayList;
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 
 @ShellComponent
+@Slf4j
 public class AutoCompleteCommand {
 
     /**
@@ -16,29 +18,32 @@ public class AutoCompleteCommand {
      */
     @ShellMethod("Autocomplete given a query string and a set of all possible query strings.")
     public List<String> autocomplete(String queryString, List<String> possibleStrings) {
+        log.debug("Checking for matching strings with prefix '{}'...", queryString);
         List<String> matchedStrings = new ArrayList<>();
         for (String possibleString : possibleStrings) {
             if (!possibleString.contains(queryString)) {
-                // not a match, possibleString must contain queryString
+                log.debug("Not a match, possible string must contain query string.");
                 continue;
             }
             if (isPrefix(queryString, possibleString)) {
                 matchedStrings.add(possibleString);
             }
         }
+        log.info("Number of matches found: {}", matchedStrings.size());
         return matchedStrings;
     }
 
     private boolean isPrefix(String possiblePrefix, String targetString) {
+        log.debug("Checking if {} is a prefix of {}", possiblePrefix, targetString);
         for (int i = 0; i < possiblePrefix.length(); i++) {
             char queryStringChar = possiblePrefix.charAt(i);
             char possibleStringChar = targetString.charAt(i);
             if (queryStringChar != possibleStringChar) {
-                // not a match, character discrepancy detected
+                log.debug("Not a match, character discrepancy detected.");
                 break;
             }
             if (i == possiblePrefix.length() - 1) {
-                // end of query string reached, match detected
+                log.debug("End of query string reached; Match detected!");
                 return true;
             }
         }
